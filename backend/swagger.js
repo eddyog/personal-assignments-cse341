ed2a50693eaa48b3468f864d875b1901158578a1
Eddy Gonzalez <gon19005@byui.edu>
2023-03-03 02:58:30 -0700

    a

--- backend/swagger.js
+++ backend/swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'Contacts API'
   },
   host: 'personal-assignment-cse341.onrender.com',
-  schemes: ['https']
+  schemes: ['http']
 };
 
 const outputFile = './swagger.json';
