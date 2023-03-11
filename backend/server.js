e499885613e6d1ee499730bbb811ed2fb1717c63
Eddy Gonzalez <gon19005@byui.edu>
2023-03-11 01:39:56 -0700

    a

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
       'Access-Control-Allow-Headers',
-      'Origin, X-Requested-Wioth, Content-Type, Accept, Z-Key'
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
     );
     res.setHeader('Content-Type', 'application/json' );
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
