dae68086ba686258274375e35b27367cff174d7d
Eddy Gonzalez <gon19005@byui.edu>
2023-03-11 02:03:46 -0700

    a

--- backend/server.js
+++ backend/server.js
@@ -5,28 +5,28 @@ const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
 const app = express();
 
-// app
-//   .use(bodyParser.json())
-//   .use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader(
-//       'Access-Control-Allow-Headers',
-//       'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-//     );
-//     res.setHeader('Content-Type', 'application/json' );
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//     next();
-//   })
-//   .use('/', require('./routes'));
-
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Content-Type', 'application/json' );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .use('/', require('./routes'));
-  
+
+// app
+//   .use(bodyParser.json())
+//   .use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     next();
+//   })
+//   .use('/', require('./routes'));
+
 
 mongodb.initDb((err) => {
   if (err) {
